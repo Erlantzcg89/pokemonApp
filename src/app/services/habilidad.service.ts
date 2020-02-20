1eca691ce09d1d37e651e2f02e570e165ed0698c
Erlantzcg89 <erlantzcg89@gmail.com>
2020-02-20 10:06:01 +0100

    habilidad service

---
+++ src/app/services/habilidad.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HabilidadService {
+
+  constructor(private http: HttpClient) {
+    console.trace("HabilidadService Constructor");
+  }// HabilidadService constructor
+
+  listar(): Observable<any[]> {
+    const url = `http://localhost:8080/pokemon-rest/api/habilidad/`;
+    console.trace('HabilidadService listar' + url);
+    return this.http.get<any[]>(url);
+  }//HabilidadService listar
+
+  detalle(id: number): Observable<any> {
+    const url = `http://localhost:8080/pokemon-rest/api/habilidad/${id}`;
+    console.trace('HabilidadService detalle' + url);
+    return this.http.get<any>(url);
+  }//HabilidadService detalle
+
+  public crear(habilidad: any): Observable<any> {
+    const url = `http://localhost:8080/pokemon-rest/api/habilidad/`;
+    console.trace('HabilidadService crear' + url);
+    return this.http.post<any>(url, habilidad);
+  }//HabilidadService crear
+
+  public modificar(habilidad: any): Observable<any> {
+    const url = `http://localhost:8080/pokemon-rest/api/habilidad/${habilidad.id}`;
+    console.trace('HabilidadService modificar' + url);
+    return this.http.put<any>(url, pokemon);
+  }//HabilidadService modificar
+
+  public borrar(id: number): Observable<any> {
+    const url = `http://localhost:8080/pokemon-rest/api/habilidad/${id}`;
+    console.trace('HabilidadService borrar' + url);
+    return this.http.delete<any>(url);
+  }// HabilidadService borrar
+
+}// HabilidadService
